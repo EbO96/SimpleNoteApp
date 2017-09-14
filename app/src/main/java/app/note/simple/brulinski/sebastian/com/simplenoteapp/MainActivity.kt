573883f7d6558bad9af6ba20fcb7f0e759f34f0d
EbO96 <sebos3maj96rok>
2017-09-14 19:50:01 +0200

    Added data binding to app and bind MainActivity layout

--- app/src/main/java/app/note/simple/brulinski/sebastian/com/simplenoteapp/MainActivity.kt
+++ app/src/main/java/app/note/simple/brulinski/sebastian/com/simplenoteapp/MainActivity.kt
@@ -1,12 +1,17 @@
 package app.note.simple.brulinski.sebastian.com.simplenoteapp
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import app.note.simple.brulinski.sebastian.com.simplenoteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
