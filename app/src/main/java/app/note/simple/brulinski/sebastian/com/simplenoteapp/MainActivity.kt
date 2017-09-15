37d128adbad6ee2d69a78037793109ea463d7d98
EbO96 <sebos3maj96rok>
2017-09-15 16:30:48 +0200

    Add function to set NotesListFragment fragment in FrameLayout

--- app/src/main/java/app/note/simple/brulinski/sebastian/com/simplenoteapp/MainActivity.kt
+++ app/src/main/java/app/note/simple/brulinski/sebastian/com/simplenoteapp/MainActivity.kt
@@ -1,56 +1,72 @@
 package app.note.simple.brulinski.sebastian.com.simplenoteapp
 
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.GridLayoutManager
 import android.view.Menu
 import android.view.MenuInflater
-import android.widget.Toast
+import android.widget.FrameLayout
 import app.note.simple.brulinski.sebastian.com.simplenoteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    lateinit var itemsObject: ItemsHolder
-    lateinit var itemsObjectsArray: ArrayList<ItemsHolder>
-    lateinit var myRecycler: MainRecyclerAdapter
-    lateinit var database: LocalDatabase
+//    lateinit var itemsObject: ItemsHolder
+//    lateinit var itemsObjectsArray: ArrayList<ItemsHolder>
+//    lateinit var myRecycler: MainRecyclerAdapter
+//    lateinit var database: LocalDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setSupportActionBar(binding.toolbar)
-        binding.recyclerView.setHasFixedSize(true)
-        binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
-
-        itemsObjectsArray = ArrayList()
-
-        myRecycler = MainRecyclerAdapter(itemsObjectsArray)
-        binding.recyclerView.adapter = myRecycler
-
-        database = LocalDatabase(this)
+//        binding.recyclerView.setHasFixedSize(true)
+//        binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
+//
+//        itemsObjectsArray = ArrayList()
+//
+//        myRecycler = MainRecyclerAdapter(itemsObjectsArray)
+//        binding.recyclerView.adapter = myRecycler
+//
+//        database = LocalDatabase(this)
 
         binding.mainFab.setOnClickListener {
             //Switch to add note fragment
+
         }
-    }
-
-    private fun addNote(title: String, note: String) {
-        itemsObject = ItemsHolder()
-
-        itemsObject.title = title
-        itemsObject.note = note
-
-        itemsObjectsArray.add(itemsObject)
-
-        myRecycler.notifyDataSetChanged()
+        try {
+            setNotesListFragment()
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
 
     }
 
-    private fun saveNoteInDatabase(title: String, note: String, date: String) {
-        database.addNote(title, note, date)
+//    private fun addNote(title: String, note: String) {
+//        itemsObject = ItemsHolder()
+//
+//        itemsObject.title = title
+//        itemsObject.note = note
+//
+//        itemsObjectsArray.add(itemsObject)
+//
+//        myRecycler.notifyDataSetChanged()
+//
+//    }
+//
+//    private fun saveNoteInDatabase(title: String, note: String, date: String) {
+//        database.addNote(title, note, date)
+//    }
+
+    fun setNotesListFragment() {
+        val fm: FragmentManager = supportFragmentManager
+        val ft: FragmentTransaction = fm.beginTransaction()
+        var notesListFragment: NotesListFragment = NotesListFragment()
+        ft.add(binding.mainContainer.id, notesListFragment).commit()
+        fm.executePendingTransactions()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -60,3 +76,4 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 }
+
--- app/src/main/java/app/note/simple/brulinski/sebastian/com/simplenoteapp/NotesListFragment.kt
+++ app/src/main/java/app/note/simple/brulinski/sebastian/com/simplenoteapp/NotesListFragment.kt
@@ -1,18 +1,50 @@
 package app.note.simple.brulinski.sebastian.com.simplenoteapp
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import app.note.simple.brulinski.sebastian.com.simplenoteapp.databinding.NotesListFragmentBinding
 
 /**
  * Created by sebas on 15.09.2017.
  */
-class NotesListFragment : Fragment(){
+class NotesListFragment : Fragment() {
+
+    lateinit var binding: NotesListFragmentBinding
+    lateinit var itemsObject: ItemsHolder
+    lateinit var itemsObjectsArray: ArrayList<ItemsHolder>
+    lateinit var myRecycler: MainRecyclerAdapter
+    lateinit var database: LocalDatabase
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val root : View = inflater!!.inflate(R.layout.notes_list_fragment, container, false)
-        return root
+        binding = DataBindingUtil.inflate(inflater, R.layout.notes_list_fragment, container, false)
+
+        try {
+            binding.recyclerView.setHasFixedSize(true)
+            binding.recyclerView.layoutManager = GridLayoutManager(context, 2)
+
+            itemsObjectsArray = ArrayList()
+
+            myRecycler = MainRecyclerAdapter(itemsObjectsArray)
+            binding.recyclerView.adapter = myRecycler
+
+            database = LocalDatabase(context)
+
+            itemsObject = ItemsHolder()
+            itemsObject.note = "test 1"
+            itemsObject.title = "test 2"
+            itemsObjectsArray.add(itemsObject)
+
+            myRecycler.notifyDataSetChanged()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+
+        return binding.root
     }
 }
