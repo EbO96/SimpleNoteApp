402cfa8c661436e0a6f5f3f67497a5b76430596b
Sebastian Bruliński <sebastian96rok@gmail.com>
2017-10-11 21:07:56 +0200

    Added delete button to delete archived notes

--- app/src/main/java/app/note/simple/brulinski/sebastian/com/simplenoteapp/RecyclerView/ArchivesRecycler.kt
+++ app/src/main/java/app/note/simple/brulinski/sebastian/com/simplenoteapp/RecyclerView/ArchivesRecycler.kt
@@ -1,5 +1,6 @@
 package app.note.simple.brulinski.sebastian.com.simplenoteapp.RecyclerView
 
+import android.app.AlertDialog
 import android.content.Context
 import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
@@ -8,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+import app.note.simple.brulinski.sebastian.com.simplenoteapp.Database.LocalSQLAnkoDatabase
+import app.note.simple.brulinski.sebastian.com.simplenoteapp.Database.database
 import app.note.simple.brulinski.sebastian.com.simplenoteapp.Editor.EditorManager
 import app.note.simple.brulinski.sebastian.com.simplenoteapp.Model.ItemsHolder
 import app.note.simple.brulinski.sebastian.com.simplenoteapp.R
@@ -25,11 +28,43 @@ class ArchivesRecycler(private val notesArrayList: ArrayList<ItemsHolder>, priva
         bg.recogniseAndSetColor(noteObject.bgColor, arrayListOf(holder.card), "BG") //Change note color
         bg.recogniseAndSetColor(noteObject.textColor, arrayListOf(holder.titleTextView, holder.noteTextView), "FONT")
 
-        holder!!.titleTextView.text = title
+        holder.titleTextView.text = title
         holder.noteTextView.text = note
 
+        var itemPosition = 0
+        var isClickedYes = false
+
         holder.deleteImageButton.setOnClickListener {
-            //TODO delete confirmation alert dialog
+
+            itemPosition = recycler.getChildAdapterPosition(holder.itemView)
+
+            val alert = AlertDialog.Builder(ctx).create()
+
+            alert.setTitle(ctx.getString(R.string.delete_this_note))
+
+            alert.setButton(AlertDialog.BUTTON_POSITIVE, ctx.getString(R.string.yes), { _, i ->
+                isClickedYes = true
+            })
+
+            alert.setButton(AlertDialog.BUTTON_NEGATIVE, ctx.getString(R.string.no), { _, i ->
+                //Do nothing
+                isClickedYes = false
+            })
+
+            alert.show()
+
+            alert.setOnDismissListener {
+                if(isClickedYes) {
+                    ctx.database.use {
+                        delete(LocalSQLAnkoDatabase.TABLE_NOTES, "${LocalSQLAnkoDatabase.ID}=?", arrayOf(noteObject.id))
+                        delete(LocalSQLAnkoDatabase.TABLE_NOTES_PROPERTIES, "${LocalSQLAnkoDatabase.NOTE_ID}=?", arrayOf(noteObject.id))
+                    }
+
+                    notesArrayList.removeAt(itemPosition)
+                    notifyItemRemoved(itemPosition)
+                }
+                isClickedYes = false
+            }
         }
 
         holder.restoreImageButton.setOnClickListener {
