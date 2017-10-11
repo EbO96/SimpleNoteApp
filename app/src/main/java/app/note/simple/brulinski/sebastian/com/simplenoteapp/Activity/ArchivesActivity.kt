e2863bfa1b5be4478b4588e660432debcd1b2582
Sebastian Bruliński <sebastian96rok@gmail.com>
2017-10-11 15:00:27 +0200

    Added empty Archives activity

---
+++ app/src/main/java/app/note/simple/brulinski/sebastian/com/simplenoteapp/Activity/ArchivesActivity.kt
@@ -0,0 +1,13 @@
+package app.note.simple.brulinski.sebastian.com.simplenoteapp.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import app.note.simple.brulinski.sebastian.com.simplenoteapp.R
+
+class ArchivesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_archives)
+    }
+}
